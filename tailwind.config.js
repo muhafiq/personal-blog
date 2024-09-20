089c8fb959738b138ef51568289965c535d35d84
muhafiq <muhafiq18@gmail.com>
2024-09-20 20:59:12 +0700

    correcting script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./views/**/*.ejs", "./public/**/*.{html,js}"],
+  content: ["./src/views/**/*.ejs", "./src/public/**/*.{html,js}"],
   theme: {
     extend: {
       keyframes: {
